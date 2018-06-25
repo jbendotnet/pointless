4246c1b6c830c66e5f80cf3898d4036555e5355d
Jon Bennett <jon.bennett@futurenet.com>
2018-06-25 16:50:47 +0100

    v4 test

--- two/get.go
+++ two/get.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/go-kit/kit/log"
+	log "github.com/sirupsen/logrus"
 )
 
 func Get() string {
-	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
-	logger.Log("two", "Get")
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.WarnLevel)
+
+	log.WithFields(log.Fields{
+		"package": "two",
+	}).Info("2")
+
 	return fmt.Sprintf("%d", 2)
 }
